7a674bda7d392e0608f8b22b0aef24e402c6156c
Tony <tonyblum@me.com>
2016-05-11 18:29:45 -0700

    basic math

--- program.cs
+++ program.cs
@@ -10,31 +10,53 @@ public class Demo
    //    Console.WriteLine("Hello " + name + "!");
    // }
 
+   // public static void Main()
+   // {
+   //    bool canVote = true;
+   //
+   //    char grade = 'A';
+   //
+   //    int maxInt = int.MaxValue;
+   //
+   //    long maxLong = long.MaxValue;
+   //
+   //    decimal maxDec = decimal.MaxValue;
+   //
+   //    float maxFloat = float.MaxValue;
+   //
+   //    double maxDouble = double.MaxValue;
+   //    Console.WriteLine("Max Int: " + maxInt);
+   //    Console.WriteLine("Max Long: " + maxLong);
+   //    Console.WriteLine("Max Dec: " + maxDec);
+   //    Console.WriteLine("Max Float: " + maxFloat);
+   //    Console.WriteLine("Max Double: " + maxDouble);
+   //
+   //    var anotherName = "Tony";
+   //
+   //    Console.WriteLine("anotherName is a {0}", anotherName.GetTypeCode());
+   // }
+
    public static void Main()
    {
-      bool canVote = true;
+      Console.WriteLine("5 + 3 = " + (5+3));
+      Console.WriteLine("5 - 3 = " + (5-3));
+      Console.WriteLine("5 * 3 = " + (5*3));
+      Console.WriteLine("5 / 3 = " + (5/3));
+      Console.WriteLine("5 % 3 = " + (5%3));
 
-      char grade = 'A';
+      int i = 0;
 
-      int maxInt = int.MaxValue;
+      Console.WriteLine("i++ = " + (i++));
+      Console.WriteLine("++i = " + (++i));
+      Console.WriteLine("i-- = " + (i--));
+      Console.WriteLine("--i = " + (--i));
 
-      long maxLong = long.MaxValue;
+      Console.WriteLine("i += 2 " + (i += 2));
+      Console.WriteLine("i -= 2 " + (i -= 2));
+      Console.WriteLine("i *= 2 " + (i *= 2));
+      Console.WriteLine("i /= 2 " + (i /= 2));
+      Console.WriteLine("i %= 2 " + (i %= 2));
 
-      decimal maxDec = decimal.MaxValue;
-
-      float maxFloat = float.MaxValue;
-
-      double maxDouble = double.MaxValue;
-      Console.WriteLine("Max Int: " + maxInt);
-      Console.WriteLine("Max Long: " + maxLong);
-      Console.WriteLine("Max Dec: " + maxDec);
-      Console.WriteLine("Max Float: " + maxFloat);
-      Console.WriteLine("Max Double: " + maxDouble);
-
-      var anotherName = "Tony";
-
-      Console.WriteLine("anotherName is a {0}", anotherName.GetTypeCode());
-      
    }
 
 }
