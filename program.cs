3e8c20d2cee10f30aebcc524ed265d3534302dff
Tony <tonyblum@me.com>
2016-05-11 19:07:34 -0700

    if and switch statements

--- program.cs
+++ program.cs
@@ -58,26 +58,72 @@ public class Demo
    //    Console.WriteLine("i %= 2 " + (i %= 2));
    // }
 
+   // public static void Main()
+   // {
+   //    double pi = 3.14;
+   //    int intPi = (int)pi;
+   //    Console.WriteLine(intPi);
+   //
+   //    double number1 = 10.5;
+   //    double number2 = 15;
+   //
+   //    Console.WriteLine("Math.Abs(number1): " + (Math.Abs(number1)));
+   //    Console.WriteLine("Math.Ceiling(number1): " + (Math.Ceiling(number1)));
+   //    Console.WriteLine("Math.Floor(number1): " + (Math.Floor(number1)));
+   //    Console.WriteLine("Math.Max(number1, number2): " + (Math.Max(number1, number2)));
+   //    Console.WriteLine("Math.Min(number1): " + (Math.Min(number1, number2)));
+   //    Console.WriteLine("Math.Pow(number1, number2): " + (Math.Pow(number1, number2)));
+   //    Console.WriteLine("Math.Round(number1): " + (Math.Round(number1)));
+   //    Console.WriteLine("Math.Sqrt(number1): " + (Math.Sqrt(number1)));
+   //
+   //    Random rand = new Random();
+   //    Console.WriteLine("Random number between 1 and 10: " + (rand.Next(1, 11)));
+   // }
+
    public static void Main()
    {
-      double pi = 3.14;
-      int intPi = (int)pi;
-      Console.WriteLine(intPi);
+      int age = 17;
 
-      double number1 = 10.5;
-      double number2 = 15;
+      if ((age >= 50) || (age <= 7))
+      {
+         Console.WriteLine("Go to elementary school.");
+      } else if ((age > 7) && (age < 13))
+      {
+         Console.WriteLine("Go to junior high school.");
+      } else
+      {
+         Console.WriteLine("Go to high school.");
+      }
 
-      Console.WriteLine("Math.Abs(number1): " + (Math.Abs(number1)));
-      Console.WriteLine("Math.Ceiling(number1): " + (Math.Ceiling(number1)));
-      Console.WriteLine("Math.Floor(number1): " + (Math.Floor(number1)));
-      Console.WriteLine("Math.Max(number1, number2): " + (Math.Max(number1, number2)));
-      Console.WriteLine("Math.Min(number1): " + (Math.Min(number1, number2)));
-      Console.WriteLine("Math.Pow(number1, number2): " + (Math.Pow(number1, number2)));
-      Console.WriteLine("Math.Round(number1): " + (Math.Round(number1)));
-      Console.WriteLine("Math.Sqrt(number1): " + (Math.Sqrt(number1)));
+      int age2 = 70;
 
-      Random rand = new Random();
-      Console.WriteLine("Random number between 1 and 10: " + (rand.Next(1, 11)));
+      if ((age2 < 14) || (age2 > 65))
+      {
+         Console.WriteLine("You should retire.");
+      }
+
+      Console.WriteLine("!true = " + (!true));
+
+      bool canDrive = age2 >= 16 ? true : false;
+
+      Console.WriteLine(canDrive);
+
+      switch (age)
+      {
+         case 0:
+            Console.WriteLine("Mars");
+            break;
+         case 1:
+         case 2:
+            Console.WriteLine("Earth");
+            goto Home;
+         default:
+            Console.WriteLine("Space");
+            break;
+      }
+
+      Home:
+      Console.WriteLine("Earth is home.");
    }
 
 }
