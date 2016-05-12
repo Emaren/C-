47bd1bd6bc1e0ee7f1d78b4dd2e651b5c6117894
Tony <tonyblum@me.com>
2016-05-12 13:47:49 -0700

    polymorphism/abstract class, interfaces, object overloading

--- program.cs
+++ program.cs
@@ -462,6 +462,61 @@ public class Demo
          }
       }
 
+      abstract class Shape
+      {
+         public abstract double area();
 
+         public void sayHi()
+         {
+            Console.WriteLine("Hello");
+         }
+      }
+
+      public interface ShapeItem
+      {
+         double area();
+      }
+
+      class Rectangle:Shape
+      {
+         private double length;
+         private double width;
+
+         public Rectangle(double num1, double num2)
+         {
+            length = num1;
+            width = num2;
+         }
+
+         public override double area()
+         {
+            return length * width;
+         }
+
+         public static Rectangle operator+ (Rectangle rect1, Rectangle rect2)
+         {
+            double rectLength = rect1.length + rect2.length;
+            double rectWidth = rect1.width + rect2.width;
+
+            return new Rectangle(rectLength, rectWidth);
+         }
+      }
+
+      class Triangle:Shape
+      {
+         private double theBase;
+         private double height;
+
+         public Triangle(double num1, double num2)
+         {
+            theBase = num1;
+            height = num2;
+         }
+
+         public override double area()
+         {
+            return .5 * (theBase * height);
+         }
+      }
 
 }
