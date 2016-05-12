0a92f06c86119606c4d2f66356fa8d755f1332fd
Tony <tonyblum@me.com>
2016-05-11 18:49:51 -0700

    Casting

--- program.cs
+++ program.cs
@@ -36,27 +36,48 @@ public class Demo
    //    Console.WriteLine("anotherName is a {0}", anotherName.GetTypeCode());
    // }
 
+   // public static void Main()
+   // {
+   //    Console.WriteLine("5 + 3 = " + (5+3));
+   //    Console.WriteLine("5 - 3 = " + (5-3));
+   //    Console.WriteLine("5 * 3 = " + (5*3));
+   //    Console.WriteLine("5 / 3 = " + (5/3));
+   //    Console.WriteLine("5 % 3 = " + (5%3));
+   //
+   //    int i = 0;
+   //
+   //    Console.WriteLine("i++ = " + (i++));
+   //    Console.WriteLine("++i = " + (++i));
+   //    Console.WriteLine("i-- = " + (i--));
+   //    Console.WriteLine("--i = " + (--i));
+   //
+   //    Console.WriteLine("i += 2 " + (i += 2));
+   //    Console.WriteLine("i -= 2 " + (i -= 2));
+   //    Console.WriteLine("i *= 2 " + (i *= 2));
+   //    Console.WriteLine("i /= 2 " + (i /= 2));
+   //    Console.WriteLine("i %= 2 " + (i %= 2));
+   // }
+
    public static void Main()
    {
-      Console.WriteLine("5 + 3 = " + (5+3));
-      Console.WriteLine("5 - 3 = " + (5-3));
-      Console.WriteLine("5 * 3 = " + (5*3));
-      Console.WriteLine("5 / 3 = " + (5/3));
-      Console.WriteLine("5 % 3 = " + (5%3));
+      double pi = 3.14;
+      int intPi = (int)pi;
+      Console.WriteLine(intPi);
 
-      int i = 0;
+      double number1 = 10.5;
+      double number2 = 15;
 
-      Console.WriteLine("i++ = " + (i++));
-      Console.WriteLine("++i = " + (++i));
-      Console.WriteLine("i-- = " + (i--));
-      Console.WriteLine("--i = " + (--i));
+      Console.WriteLine("Math.Abs(number1): " + (Math.Abs(number1)));
+      Console.WriteLine("Math.Ceiling(number1): " + (Math.Ceiling(number1)));
+      Console.WriteLine("Math.Floor(number1): " + (Math.Floor(number1)));
+      Console.WriteLine("Math.Max(number1, number2): " + (Math.Max(number1, number2)));
+      Console.WriteLine("Math.Min(number1): " + (Math.Min(number1, number2)));
+      Console.WriteLine("Math.Pow(number1, number2): " + (Math.Pow(number1, number2)));
+      Console.WriteLine("Math.Round(number1): " + (Math.Round(number1)));
+      Console.WriteLine("Math.Sqrt(number1): " + (Math.Sqrt(number1)));
 
-      Console.WriteLine("i += 2 " + (i += 2));
-      Console.WriteLine("i -= 2 " + (i -= 2));
-      Console.WriteLine("i *= 2 " + (i *= 2));
-      Console.WriteLine("i /= 2 " + (i /= 2));
-      Console.WriteLine("i %= 2 " + (i %= 2));
-
+      Random rand = new Random();
+      Console.WriteLine("Random number between 1 and 10: " + (rand.Next(1, 11)));
    }
 
 }
